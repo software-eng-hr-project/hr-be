f74c05f6adf3c4e81f6e8d963f177446770f6ca4
Whobreak <buyukserbetcieray@gmail.com>
2024-12-13 23:52:52 +0300

    fix: fixed the error when create a new user who is add before

--- src/ProjectHr.Core/Common/Errors/ErrorCodeHelpers.cs
+++ src/ProjectHr.Core/Common/Errors/ErrorCodeHelpers.cs
@@ -7,28 +7,30 @@ public static class ErrorCodeHelpers
 {
     public static void DuplicateMessageHelper(Exception e)
     {
-        var message = e.InnerException.Message;
-        
+        string message = "";
+        if (e.InnerException is not null)
+            message = e.InnerException.Message;
+
         if (message.Contains("duplicate"))
         {
             ErrorCode errorCode = ErrorCode.DuplicateError;
-            
+
             if (message.Contains("WorkEmailAddress"))
                 errorCode = ErrorCode.WorkEmailAdressUnique;
-            
+
             else if (message.Contains("WorkPhone"))
                 errorCode = ErrorCode.WorkPhoneUnique;
-            
+
             else if (message.Contains("PersonalPhone"))
                 errorCode = ErrorCode.PersonalPhoneUnique;
-            
+
             else if (message.Contains("IdentityNumber"))
                 errorCode = ErrorCode.IdentityNumberUnique;
-            
+
             else if (message.Contains("EmergencyContactPhone"))
                 errorCode = ErrorCode.EmergencyPhoneUnique;
-            
-                throw ExceptionHelper.Create(errorCode);
+
+            throw ExceptionHelper.Create(errorCode);
         }
     }
 }
