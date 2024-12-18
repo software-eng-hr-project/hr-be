b105a7c362b99f2edf0d8f38068784a0de500326
Safa Gençtorun <safa_genctorun@hotmail.com>
2024-12-18 14:04:48 +0300

    feat: remove TeamName column from ProjectMembers table

---
+++ src/ProjectHr.EntityFrameworkCore/Migrations/20230905071211_team name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectHr.Migrations
+{
+    /// <inheritdoc />
+    public partial class teamname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamName",
+                table: "ProjectMembers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TeamName",
+                table: "ProjectMembers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
